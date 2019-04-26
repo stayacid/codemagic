0670eee878672d942b8441429068e3890394c40e
stayacid <stayacid@gmail.com>
2019-04-26 11:25:08 +0300

    new start

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
-'use strict';
+var fireballSize = 22,
+    wizardSpeed = 3,
+    wizardWidth = 70
 
-(function () { //IIFE для модуля
-    var fireballSize = 22,
-        wizardSpeed = 3,
-        wizardWidth = 70;
+var getFireballSpeed = function(left) {
+  if (left) {
+    return 5;
+  }
+  return 2;
+}
 
-    function getWizardHeigth(wizardWidth) {
-        return 1.337 * wizardWidth;
-    }
-
-    function getFireballSpeed(left) {
-        var result = left ? 5 : 2;
-        return result;
-    }
-
-    function getWizardX(width) {
-        return width / 2;
-    }
-
-    function getWizardY(height) {
-        return height / 3 * 2;
-    }
-}());
+var getWizardHeight = function() {
+  return 1.337 * wizardWidth;
+}
