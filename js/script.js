0b86779d7399e94a8a57f6dfafeecd6bf2521123
stayacid <stayacid@gmail.com>
2019-04-26 11:32:34 +0300

    refactor

--- js/script.js
+++ js/script.js
@@ -2,11 +2,8 @@ var fireballSize = 22,
     wizardSpeed = 3,
     wizardWidth = 70
 
-var getFireballSpeed = function(left) {
-  if (left) {
-    return 5;
-  }
-  return 2;
+var getFireballSpeed = function (left) {
+  return (left) ? 5 : 2;
 }
 
 var getWizardHeight = function() {
