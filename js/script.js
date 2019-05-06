bdead421c1315d80c933a2960529a2849ce5869f
stayacid <stayacid@gmail.com>
2019-05-06 18:01:39 +0300

    1 lesson done

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ var fireballSize = 22,
 
 var getFireballSpeed = function (left) {
   return (left) ? 5 : 2;
-}
+};
 
 var getWizardHeight = function() {
   return 1.337 * wizardWidth;
-}
+};
+
+var getWizardX = function(width) {
+  return width / 2 - wizardWidth / 2 ;
+};
+
+var getWizardY = function(height) {
+  return height * 2 / 3;
+};
