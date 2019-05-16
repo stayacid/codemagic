7e5681c7f1a91a25acb73e8313a4fd5ca6dc60ac
stayacid <stayacid@gmail.com>
2019-05-16 16:58:22 +0300

    add graphics

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,20 @@
 'use strict';
 
-window.renderStatistics = function(ctx, names, times) {
+var getMaxElement = function(arr) {
+  
+}
+
+window.renderStatistics = function (ctx, names, times) {
+  //position in px
+  var 
+      initialX = 120, //horizontal text positioning
+      initialY = 40,
+      max = -1,
+      barHeight = 20,
+      indent = 40,
+      lineHeight = 15,
+      maxIndex = -1;
+
   //shadow
   ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
   ctx.fillRect(110, 20, 420, 270);
@@ -12,35 +26,40 @@ window.renderStatistics = function(ctx, names, times) {
   //text
   ctx.fillStyle = "#000";
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText('Ура вы победили!', initialX, initialY);
 
-  //
-  var h = 100, //vertical text positioning
-      max = -1,
-      maxIndex = -1;
+  //move line down
+  initialY += barHeight;
+  
+  ctx.fillText('Список результатов:', initialX, initialY);
+
 
   //draw list of results from [times]
-  for(var i = 0; i < times.length; i++) {
-    var time  = times[i];
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
     if (time > max) {
       max = time; //time
       maxIndex = i; // index of max value in [times]
     }
-
-    h += 20;
-    //ctx.fillText(time, 120, h);
-    //dfg
-
   };
 
   //
   var histogramWidth = 150,
-  step = histogramWidth / (max - 0); //replace zero by minimum value from [times]
+    step = histogramWidth / (max - 0); //replace zero by minimum value from [times]
 
-  ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 80);
+  //move line down
+  initialY += barHeight;
 
-  
+  ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], initialX, initialY);
 
+  //move line down
+  initialY += lineHeight;
+
+  //draw graphic
+  for(var i = 0; i < times.length; i++) {
+    initialY += 5; 
+    ctx.fillRect(initialX, initialY + indent * i, times[i] * step, barHeight);
+    ctx.fillText(names[i], initialX + histogramWidth + barHeight, initialY  + barHeight + indent * i);
+  }
 
 };
