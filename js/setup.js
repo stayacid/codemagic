05ca7ddb5640851323175c74ad30d0d91b981acd
stayacid <stayacid@gmail.com>
2019-05-30 12:11:34 +0300

    add similar wizards in modal

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,46 @@
 'use strict';
 
-document.querySelector('.setup').classList.remove('hidden');
+// close and open window
+const setupBtn = document.querySelector('.setup-open');
+const closeBtn = document.querySelector('.setup-close');
 
-let names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-let surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-let coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-let eyesColors = ['black', 'blue', 'red', 'yellow', 'green'];
+
+setupBtn.addEventListener('click', () => {
+  toggleModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  toggleModal();
+});
+
+const toggleModal = () => {
+  document.querySelector('.overlay').classList.toggle('hidden');
+  document.querySelector('.setup-similar').classList.toggle('hidden');
+};
+
+// clone and append template to similar list
+const similarListElement = document.querySelector('.setup-similar-list');
+const similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+const names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+const surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+const coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+const eyesColors = ['black', 'blue', 'red', 'yellow', 'green'];
+
+// get random number
+const randomNumber = (min, max) => {
+  let rand = min + Math.random() * (max.length - min);
+  rand = Math.floor(rand);
+  return rand;
+};
+
+const fragment = document.createDocumentFragment();
+for (let i = 0; i < 4; i++) {
+  const wizardElement = similarWizardTemplate.cloneNode(true);
+  // append random name and surname
+  wizardElement.querySelector('.setup-similar-label').textContent = names[randomNumber(0, names)] + ' ' + surnames[randomNumber(0, surnames)];
+  // add random color
+  wizardElement.querySelector('.wizard-coat').style.fill = coatColors[randomNumber(0, coatColors)];
+  // append template
+  fragment.appendChild(wizardElement);
+};
+similarListElement.appendChild(fragment);
