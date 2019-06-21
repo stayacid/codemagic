0ae871367ee0bf8c5cf7c51b1b193f77c21faa22
Ivan Zagainov <stayacid@gmail.com>
2019-06-21 11:34:19 +0300

    add color change on click

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,28 @@
 'use strict';
 
+// get random number
+const randomNumber = (min, max) => {
+  let rand = min + Math.random() * (max - min);
+  rand = Math.floor(rand);
+  return rand;
+};
+
 // close and open window
 const setupBtn = document.querySelector('.setup-open');
 const closeBtn = document.querySelector('.setup-close');
-const enterKeyCode = 13;
-const escKeyCode = 27;
+const setupModal = document.querySelector('.setup');
+const keyCodes = {
+  enter: 13,
+  esc: 27,
+};
 
-
-// open modal
+// open and close modal
 setupBtn.addEventListener('click', () => {
   toggleModal();
 });
 
 setupBtn.addEventListener('keydown', (e) => {
-  if (e.keyCode === enterKeyCode) {
+  if (e.keyCode === keyCodes.enter) {
     toggleModal();
   }
 });
@@ -23,13 +32,13 @@ closeBtn.addEventListener('click', () => {
 });
 
 closeBtn.addEventListener('keydown', (e) => {
-  if (e.keyCode === enterKeyCode) {
+  if (e.keyCode === keyCodes.enter) {
     closeModal();
   }
 });
 
 document.addEventListener('keydown', (e) => {
-  if (e.keyCode === escKeyCode) {
+  if (e.keyCode === keyCodes.esc) {
     closeModal();
   }
 });
@@ -38,10 +47,42 @@ const toggleModal = () => {
   document.querySelector('.overlay').classList.toggle('hidden');
 };
 
+// prevent closing modal if input is active
 const closeModal = () => {
-  document.querySelector('.overlay').classList.add('hidden');
+  if (setupModal.querySelector('.setup-user-name') !== document.activeElement) {
+    document.querySelector('.overlay').classList.add('hidden');
+  } return false;
 };
 
+// changing colors on click on wizard
+// change color of item by random number from array
+let arrCounter = 0;
+const changeFillColor = (item, arr) => {
+  item.style.fill = arr[arrCounter];
+  arrCounter++;
+  arrCounter >= arr.length ? arrCounter = 0 : false;
+};
+const changeBgColor = (item, arr) => {
+  item.style.background = arr[randomNumber(0, arr.length - 1)];
+};
+
+// change coat color on click
+const coatColor = setupModal.querySelector('.setup-wizard .wizard-coat');
+coatColor.addEventListener('click', function() {
+  changeFillColor(coatColor, coatColors);
+});
+
+// changing eyes color on click
+const eyesColor = setupModal.querySelector('.setup-wizard .wizard-eyes');
+eyesColor.addEventListener('click', function() {
+  changeFillColor(eyesColor, eyesColors);
+});
+
+// changing fireball color on click
+const fireballColor = setupModal.querySelector('.setup-fireball-wrap');
+fireballColor.addEventListener('click', function() {
+  changeBgColor(fireballColor, fireballColors);
+});
 
 
 // clone and append template to similar list
@@ -52,23 +93,17 @@ const names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кри�
 const surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 const coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 const eyesColors = ['black', 'blue', 'red', 'yellow', 'green'];
-
-// get random number
-const randomNumber = (min, max) => {
-  let rand = min + Math.random() * (max.length - min);
-  rand = Math.floor(rand);
-  return rand;
-};
+const fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', ' #e848d5', '#e6e848'];
 
 const fragment = document.createDocumentFragment();
 for (let i = 0; i < 4; i++) {
   const wizardElement = similarWizardTemplate.cloneNode(true);
   // append random name and surname
-  wizardElement.querySelector('.setup-similar-label').textContent = names[randomNumber(0, names)] + ' ' + surnames[randomNumber(0, surnames)];
+  wizardElement.querySelector('.setup-similar-label').textContent = names[randomNumber(0, names.length)] + ' ' + surnames[randomNumber(0, surnames.length)];
   // add random coat color
-  wizardElement.querySelector('.wizard-coat').style.fill = coatColors[randomNumber(0, coatColors)];
+  wizardElement.querySelector('.wizard-coat').style.fill = coatColors[randomNumber(0, coatColors.length)];
   // add random eyes color
-  wizardElement.querySelector('.wizard-eyes').style.fill = eyesColors[randomNumber(0, eyesColors)];
+  wizardElement.querySelector('.wizard-eyes').style.fill = eyesColors[randomNumber(0, eyesColors.length)];
   // append template
   fragment.appendChild(wizardElement);
 };
